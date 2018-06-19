7aee6eb12226330476c8534ff331ba299190663d
thealphadollar <shivam.cs.iit.kgp@gmail.com>
2018-06-19 12:41:05 +0530

    Add icon in multiple dimensions

--- kgp_erp_extension/background.js
+++ kgp_erp_extension/background.js
@@ -4,7 +4,6 @@ function install_notice() {
 
     var now = new Date().getTime();
     localStorage.setItem('install_time', now);
-    chrome.browserAction.setIcon({path:"images/icon.jpg"});
     chrome.tabs.create({url: "options.html"});
 }
 install_notice();
